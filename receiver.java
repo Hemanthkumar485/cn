63ade0142ed03b5c8e7f7986200f7cf44ae14f04
Hemanth Kumar B <165136617+Hemanthkumar485@users.noreply.github.com>
2025-11-13 21:22:07 +0530

    Update receiver.java

--- receiver.java
+++ receiver.java
@@ -1,37 +1,40 @@
 import java.net.*;
 import java.io.*;
-class slidreceiver
-{
-public static void main(String a[])throws Exception
-{
-Socket s=new Socket(InetAddress.getLocalHost(),10);
-DataInputStream in=new DataInputStream(s.getInputStream());
-PrintStream p=new PrintStream(s.getOutputStream());
-int i=0,rptr=-1,nf,rws=8;
-String rbuf[]=new String[8];
-String ch;
-System.out.println("Receiver");
-do
-{
-nf=Integer.parseInt(in.readLine());
-if(nf<=rws-1)
-{
-for(i=1;i<=nf;i++)
-{
-"+rbuf[rptr]);
-rptr=++rptr%8;
-rbuf[rptr]=in.readLine();
-System.out.println("The
-received Frame " +rptr+" is :
-}
-PA
-GE
-rws-=nf; System.out.println("\nAcknowledgment sent\n"); 1
-p.println(rptr+1); rws+=nf;
-}
-else break;
-ch=in.readLine();
-}
-while(ch.equals("yes"));
-}
+
+public class SlidReceiver {
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket(InetAddress.getLocalHost(), 5000);
+        DataInputStream in = new DataInputStream(s.getInputStream());
+        PrintStream p = new PrintStream(s.getOutputStream());
+
+        int i, rptr = -1, nf, rws = 8;
+        String rbuf[] = new String[8];
+        String ch;
+
+        System.out.println("Receiver started...");
+
+        do {
+            nf = Integer.parseInt(in.readLine());
+            if (nf <= rws - 1) {
+                for (i = 1; i <= nf; i++) {
+                    rptr = ++rptr % 8;
+                    rbuf[rptr] = in.readLine();
+                    System.out.println("Received Frame " + rptr + ": " + rbuf[rptr]);
+                }
+
+                rws -= nf;
+                System.out.println("\nAcknowledgment sent for frame " + (rptr + 1) + "\n");
+                p.println(rptr + 1);
+                rws += nf;
+            } else {
+                System.out.println("The number of frames exceeds the receiver window size.");
+                break;
+            }
+
+            ch = in.readLine();
+        } while (ch.equalsIgnoreCase("yes"));
+
+        s.close();
+        System.out.println("Connection closed.");
+    }
 }
